ef73251fcb0d39bc06400498297c569ae1f3e216
unknown <hamza23@seznam.cz>
2023-12-17 18:15:32 +0100

    Added change password and color coordinated task priority

--- ToDoApp.py
+++ ToDoApp.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, flash, redirect, url_for, request, session
-from forms import SignUpForm, LogInForm, TodoForm
+from forms import SignUpForm, LogInForm, TodoForm,  Enter_EmailForm, Reset_PasswordForm
+from flask_mail import Mail, Message
 from flask_pymongo import PyMongo
+import jwt
+import datetime
 
 App = Flask(__name__)
 App.config["SECRET_KEY"] = "your Secret Key"
@@ -9,6 +12,14 @@ App.config["MONGO_URI"] = "your link to your mongodb"
 mongo = PyMongo(App)
 user = mongo.db.users #users is a name of collection in my db
 
+App.config['MAIL_SERVER'] = 'smtp.gmail.com'
+App.config['MAIL_PORT'] = 465
+App.config['MAIL_USERNAME'] = "Your email"
+App.config['MAIL_PASSWORD'] = "the password of your email"
+App.config['MAIL_USE_TLS'] = False
+App.config['MAIL_USE_SSL'] = True
+mail = Mail(App)
+
 tasks = []
 
 def update_db(updated_tasks):
@@ -16,6 +27,9 @@ def update_db(updated_tasks):
     current_user = user.find_one({"username": session["curr_user"]})
     user.update_one(current_user, new_tasks)
 
+def token_gen(email):
+    token = jwt.encode({'email': email, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=3)}, App.config["SECRET_KEY"], algorithm="HS256")
+    return token
 
 #pages
 @App.route("/")
@@ -25,37 +39,68 @@ def home():
         return redirect(url_for("Mainpage"))
     else:
         return render_template("HomePage.html")
+    
 @App.route("/login", methods=["GET","POST"])
 def Login():
     form = LogInForm()
     if "curr_user" in session:
         return redirect(url_for("Mainpage"))
-    login_user = user.find_one({"username": form.username.data})
-    if login_user is not None:
-        if form.password.data == login_user["password"]:
-            if form.checkbox.data is True:
-                session["curr_user"] = form.username.data
-                session.permanent = True
+    if request.method == "POST":
+        lower_username = form.username.data.lower()
+        login_user = user.find_one({"username": lower_username})
+        if login_user is not None:
+            if form.password.data == login_user["password"]:
+                session["curr_user"] = lower_username
+                if form.checkbox.data is True:
+                    session.permanent = True
+                else:
+                    session.permanent = False
+                return redirect(url_for("Mainpage"))
             else:
-                session["curr_user"] = form.username.data
-                session.permanent = False
-            return redirect(url_for("Mainpage"))
-        else:
-            flash("Wrong credentials!")  
+                flash("Wrong credentials!")
     return render_template("LogIn.html", form = form) 
-    
+
+@App.route("/password_reset", methods=["GET", "POST"])
+def enter_email():
+    form = Enter_EmailForm()
+    if form.email.data is not None:
+        lower_email = form.email.data.lower()
+        existing_email = user.find_one({"email": lower_email})
+    if request.method == "POST" and existing_email is not None:
+        temp_link = token_gen(lower_email)
+        message = Message("Password change request", sender="NoReply@todoapp.com",recipients=[lower_email]) #sender somehow is the same as the email
+        message.html = f'To reset your password, click the following link: <a href="{url_for("change_password", token = temp_link, _external=True)}">Reset Password</a>'
+        mail.send(message)
+        return redirect(url_for("Login"))
+    return render_template("EnterEmail.html", form = form)
+
+
+@App.route("/change_password/<token>", methods=["GET", "POST"])
+def change_password(token):
+    form = Reset_PasswordForm()
+    if request.method == "POST":
+        try:
+            current_email = jwt.decode(token, App.config["SECRET_KEY"], algorithms=["HS256"])
+        except jwt.ExpiredSignatureError:
+            return "<h1> Token has expired </h1>"
+        new_password = {"$set":{"password": form.password.data}}
+        current_user = user.find_one({"email": current_email["email"]})
+        user.update_one(current_user, new_password)
+        return redirect(url_for("Login"))
+    return render_template("ChangePassword.html", form = form)
+
 @App.route("/signup", methods=["GET", "POST"])
 def Signup():
     form = SignUpForm()
     if "curr_user" in session:
         return redirect(url_for("Mainpage"))
-    if form.email.data is not None:
-        lower_email = form.email.data.lower()
     if request.method == "POST":
-        existing_user = user.find_one({"username": form.username.data})
+        lower_email = form.email.data.lower()
+        lower_username = form.username.data.lower()
+        existing_user = user.find_one({"username": lower_username})
         existing_email = user.find_one({"email": lower_email})
         if existing_user is None and existing_email is None:
-            user.insert({"username": form.username.data , "email": lower_email ,"password": form.password.data, "tasks":tasks})
+            user.insert_one({"username": lower_username , "email": lower_email ,"password": form.password.data, "tasks":tasks})
             flash("Successfully signed up")
             return redirect(url_for("Login"))
         elif existing_user is not None:
@@ -113,7 +158,8 @@ def priority(index):
     task = tasks[index]
     task["priority"] = form.priority.data
     sorted_tasks = sorted(tasks, key=lambda x: x["priority"], reverse=True)
-    update_db(sorted_tasks)
+    tasks[:] = sorted_tasks
+    update_db(tasks)
     return redirect(url_for("Mainpage"))
 
 @App.route("/logout")
@@ -122,13 +168,7 @@ def logout():
         flash("You have been successfully logged out")
     session.pop("curr_user")
     return redirect(url_for("home"))
-
-@App.route("/remember")
-def remember():
-    print("remember")
-
 #buttons
-
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class SignUpForm(FlaskForm):
@@ -14,6 +14,14 @@ class LogInForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired(), Length(max=20)])
     checkbox = BooleanField("Remember account?")
     submit = SubmitField("Log In")
+
+class Enter_EmailForm(FlaskForm):
+    email = StringField(validators=[DataRequired(), Email()])
+    send = SubmitField("Send")
+class Reset_PasswordForm(FlaskForm):
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8, max=20)])
+    confirm_password = PasswordField("Confirm password", validators=[DataRequired(), EqualTo("password"), Length(max=20)])
+    confirm = SubmitField("Confirm")
 
 class TodoForm(FlaskForm):
     user_input = StringField(validators=[DataRequired(), Length(max=50)])
